7d1b10aaec93b0f6ed3a13d15e04cb2819bb29fc
Chen Eini <chen.eini000@gmail.com>
2020-01-15 19:56:02 +0200

    add new pages

--- BigMama/RecipesViewController.swift
+++ BigMama/RecipesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecipesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +17,3 @@ class ViewController: UIViewController {
 
 
 }
-
