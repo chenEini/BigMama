048e3386c333f4dfd2cbfab2dbe77b6ffacbc20e
chenEini <chen.eini000@gmail.com>
2020-04-12 21:46:31 +0300

    add recipes list

--- BigMama/RecipesFeedViewController.swift
+++ BigMama/RecipesFeedViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class RecipesFeedViewController: UIViewController {
+class RecipesFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var data = [Recipe]()
+    
+    var pancakes = Recipe(id: "1", owner: User(id: "1"))
+    var cake = Recipe(id: "2", owner: User(id: "2"))
+    var maffin = Recipe(id: "3", owner: User(id: "3"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        data.append(pancakes)
+        data.append(cake)
+        data.append(maffin)
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath)
+        
+        return cell
+    }
 }
