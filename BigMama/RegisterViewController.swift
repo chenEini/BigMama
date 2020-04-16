8924098ee4d59e56395e996595fa2a8b5482571c
Kamil Beckenstein <kamilbecken@gmail.com>
2020-04-16 23:39:12 +0300

    add picture functionallty in registration

--- BigMama/RegisterViewController.swift
+++ BigMama/RegisterViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     @IBOutlet weak var nameTv: UITextField!
     @IBOutlet weak var emailTv: UITextField!
     @IBOutlet weak var pwdTv: UITextField!
     @IBOutlet weak var msgLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     
     override func viewDidLoad(){
         super.viewDidLoad()
@@ -22,23 +25,54 @@ class RegisterViewController: UIViewController {
         msgLabel.alpha = 0
     }
     
+    var selectedImage:UIImage?
+    
+    @IBAction func image(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage;
+        self.imageView.image = selectedImage;
+        dismiss(animated: true, completion: nil);
+    }
+    
     @IBAction func register(_ sender: UIButton){
+        
         let error = validateFields()
         if error != nil{
             showMsg(error!)
         }
         else{
             let new_user = User(name: nameTv.text!, email: emailTv.text!, pwd: pwdTv.text!)
-            Model.instance.register(user:new_user){(success) in
-                if(success){
-                    self.navigationController?.popViewController(animated: true)
-                    self.navigationController?.popViewController(animated: true)
-                }
-                else{
-                    showMsg("Registration failed")
-                }
+            guard let selectedImage = selectedImage else {
+                self.regAddition(user: new_user)
+                return
+            }
+            
+            Model.instance.saveImage(image: selectedImage) {(url) in
+                new_user.avatar = url
+                self.regAddition(user: new_user)
             }
         }
+    }
+      
+    func regAddition(user:User){
+        Model.instance.register(user:user){(success) in
+        if(success){
+            self.navigationController?.popViewController(animated: true)
+            self.navigationController?.popViewController(animated: true)
+        }
+        else
+        {
+            showMsg("Registration failed")
+        }}
     }
     
     func validateFields() -> String?{
